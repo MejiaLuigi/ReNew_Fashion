7b554f7e795ef6bf461472b391896717fd82fdad
santiago suarez <santiasuarezbuitrago@gmail.com>
2023-06-26 12:22:48 -0500

    Se crea el controlador

--- js/controlador.js
+++ js/controlador.js
@@ -47,6 +47,29 @@ function closeDescriptionModal() {
 
 
 // Formulario
+// const btnLanzarmodal = document.querySelector('#lanzar-modal');
+// const btnOcultarmodal = document.querySelector('#ocultar-modal');
+
+// const contModal = document.querySelector('.contenedor-modal');
+
+// const nombreInput = document.querySelector('#nombre');
+// const apellidosInput = document.querySelector('#apellidos');
+// const edadInput = document.querySelector('#edad');
+// const departamentoInput = document.querySelector('#departamento');
+// const ciudadInput = document.querySelector('#ciudad');
+// const siInput = document.querySelector('#si');
+// const noInput = document.querySelector('#no');
+
+// btnLanzarmodal.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     contModal.classList.add('mostrar');
+
+// });
+
+// btnOcultarmodal.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     contModal.classList.remove('mostrar');
+// })
 const btnLanzarmodal = document.querySelector('#lanzar-modal');
 const btnOcultarmodal = document.querySelector('#ocultar-modal');
 
@@ -63,99 +86,315 @@ const noInput = document.querySelector('#no');
 btnLanzarmodal.addEventListener('click', (e) => {
     e.preventDefault();
     contModal.classList.add('mostrar');
-
 });
 
 btnOcultarmodal.addEventListener('click', (e) => {
     e.preventDefault();
     contModal.classList.remove('mostrar');
-})
+});
 
-inputenviar.addEventListener('click', (evento) => {
-  evento.preventDefault();
-  let usuario = nombre.value; 
-    let lastname = apellidos.value; 
-    let email = correo.value; 
-    let telefono = celular.value; 
-    let years = edad.value; 
-    let depart = departamento.value; 
-    let residencia = ciudad.value; 
-    let exper_si = si.value; 
-    let exper_no = no.value;
 
-    let errores=[];
-    let datos;
-    datosMemoria= JSON.parse(localStorage.getItem('formulario'));
-    
-    if(!datosMemoria) {
-      datos=[];
-  } else {
-      datos = datosMemoria; 
-  }
+// Para validar
 
-  if(!usuario){
-    errores.push('El nombre debe ser obligatorio'); 
-    campoNombre.classList.add("error");
-    campoNombre.classList.add("is-invalid"); 
-    document.getElementById('errorUsuario').textContent = 'El campo nombre debe ser obligatorio';
-    } else if(usuario.length < 10) {
-        campoNombre.classList.add("is-invalid")
-        document.getElementById('errorUsuario').textContent = 'El Nombre debe tener minimo 10 caracteres';    
+const formulario = document.querySelector('form');
+
+/*formulario.addEventListener('submit', (evento) => {
+  evento.preventDefault();*/
+
+  /*const usuario = formulario.nombre.value;
+  const apellido = formulario.apellidos.value;
+  const email = formulario.correo.value;
+  const telefono = formulario.celular.value;
+  const edad = formulario.edad.value;
+  const documento = formulario.documento.value;
+  const departamento = formulario.departamento.value;
+  const residencia = formulario.ciudad.value;
+  const experiencia = formulario.experiencia.value;
+  const archivo = formulario.archivo.files[0];*/
+
+  formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+  
+    const campoNombre = document.getElementById('nombre');
+    const campoApellido = document.getElementById('apellidos');
+    const campoEmail = document.getElementById('correo');
+    const campoTelefono = document.getElementById('celular');
+    const campoFecha = document.getElementById('edad');
+    const campoDocumento = document.getElementById('documento');
+    const campoDepartamento = document.getElementById('departamento');
+    const campoResidencia = document.getElementById('ciudad');
+    const campoArchivo = document.getElementById('archivo');
+  
+    const usuario = campoNombre.value;
+    const apellido = campoApellido.value;
+    const email = campoEmail.value;
+    const telefono = campoTelefono.value;
+    const edad = campoFecha.value;
+    const documento = campoDocumento.value;
+    const departamento = campoDepartamento.value;
+    const residencia = campoResidencia.value;
+    const experiencia = formulario.experiencia.value;
+    const archivo = campoArchivo.files[0];
+  
+    let errores = [];
+  
+    if (!usuario) {
+      errores.push('El nombre debe ser obligatorio');
+      campoNombre.classList.add('is-invalid');
+      document.getElementById('errorUsuario').textContent = 'El campo nombre debe ser obligatorio';
+    } else if (usuario.length < 10) {
+      campoNombre.classList.add('is-invalid');
+      document.getElementById('errorUsuario')
+    } else {
+      campoNombre.classList.remove('is-invalid');
     }
-
-    if(!lastname){
-      errores.push('El apellido debe ser obligatorio'); 
-      campoApellido.classList.add("is-invalid")
+  
+    if (!apellido) {
+      errores.push('El apellido debe ser obligatorio');
+      campoApellido.classList.add('is-invalid');
       document.getElementById('errorApellido').textContent = 'El campo apellido debe ser obligatorio';
-  } else if (lastname.length < 10) {
-      campoApellido.classList.add("is-invalid")
-      document.getElementById('errorApellido').textContent = 'El apellido debe tener minimo 10 caracteres';    
-  }
+    } else {
+      campoApellido.classList.remove('is-invalid');
+    }
+  
+    if (!email) {
+      errores.push('El correo debe ser obligatorio');
+      campoEmail.classList.add('is-invalid');
+      document.getElementById('errorCorreo').textContent = 'El campo correo debe ser obligatorio';
+    } else {
+      campoEmail.classList.remove('is-invalid');
+    }
+  
+    if (!telefono) {
+      errores.push('El teléfono debe ser obligatorio');
+      campoTelefono.classList.add('is-invalid');
+      document.getElementById('errorTel').textContent = 'El campo Teléfono debe ser obligatorio';
+    } else {
+      campoTelefono.classList.remove('is-invalid');
+    }
+  
+    if (!edad) {
+      errores.push('La edad debe ser obligatoria');
+      campoFecha.classList.add('is-invalid');
+      document.getElementById('errorEdad').textContent = 'El campo edad debe ser obligatorio';
+    } else {
+      campoFecha.classList.remove('is-invalid');
+    }
+  
+    if (!departamento) {
+      errores.push('El Departamento debe ser obligatorio');
+      campoDepartamento.classList.add('is-invalid');
+      document.getElementById('errorDepart').textContent = 'El campo Departamento debe ser obligatorio';
+    } else {
+      campoDepartamento.classList.remove('is-invalid');
+    }
+  
+    if (!residencia) {
+      errores.push('La ciudad de residencia debe ser obligatoria');
+      campoResidencia.classList.add('is-invalid');
+      document.getElementById('errorResidencia').textContent = 'El campo Ciudad debe ser obligatorio';
+    } else {
+      campoResidencia.classList.remove('is-invalid');
+    }
+  
+    if (!experiencia) {
+      errores.push('La experiencia debe ser obligatoria');
+    }
+  
+    if (!archivo) {
+      errores.push('El archivo adjunto debe ser obligatorio');
+      campoArchivo.classList.add('is-invalid');
+      document.getElementById('errorArchivo').textContent = 'El campo archivo adjunto debe ser obligatorio';
+    } else {
+      campoArchivo.classList.remove('is-invalid');
+    }
+  
+    if (errores.length > 0) {
+      // Si hay errores, puedes mostrarlos al usuario o realizar otra acción necesaria.
+      for (let i = 0; i < errores.length; i++) {
+        console.log(errores[i]); // Muestra cada error en la consola
+      }
+    } else {
+      // Si no hay errores, puedes proceder con el envío del formulario o realizar otras acciones necesarias.
+      const datosEnvio = {
+        usuario,
+        apellido,
+        email,
+        telefono,
+        edad,
+        documento,
+        departamento,
+        residencia,
+        experiencia,
+        archivo,
+      };
+  
+      let datosMemoria = JSON.parse(localStorage.getItem('formulario')) || [];
+      datosMemoria.push(datosEnvio);
+      localStorage.setItem('formulario', JSON.stringify(datosMemoria));
+  
+      formulario.reset();
+    }
+  });
+  
+  
 
-  if(!email){
-    errores.push('El correo debe ser obligatorio'); 
-    campoEmail.classList.add("is-invalid")
-    document.getElementById('errorCorreo').textContent = 'El campo correo debe ser obligatorio';
-}  else if (!validarEmail(email)) {
-    errores.push('El correo debe ser obligatorio')
-    campoEmail.classList.add("is-invalid")
-    document.getElementById('errorCorreo').textContent = 'El correo electrónico no es válido';
-}
 
-if(!telefono){
-  errores.push('El telefono debe ser obligatorio'); 
-  campoTelefono.classList.add("is-invalid")
-  document.getElementById('errorTel').textContent = 'El campo Telefono debe ser obligatorio';
-  }
+ /* const datosEnvio = {
+    usuario,
+    apellido,
+    email,
+    telefono,
+    edad,
+    documento,
+    departamento,
+    residencia,
+    experiencia,
+    archivo,
+  };
 
-  if(!years){
-    errores.push('La edad debe ser obligatoria'); 
-    campoFecha.classList.add("is-invalid")
-    document.getElementById('errorEdad').textContent = 'El campo edad debe ser obligatorio';
- }
+  let datosMemoria = JSON.parse(localStorage.getItem('formulario')) || [];
+  datosMemoria.push(datosEnvio);
+  localStorage.setItem('formulario', JSON.stringify(datosMemoria));
 
- if(!depart){
-  errores.push('La Departamento debe ser obligatoria'); 
-  campoFecha.classList.add("is-invalid")
-  document.getElementById('errorDepart').textContent = 'El campo Departamento debe ser obligatorio';
-}
+  formulario.reset();*/
+/*});*/
 
-if(!residencia){
-  errores.push('La ciudadDepartamento debe ser obligatoria'); 
-  campoFecha.classList.add("is-invalid")
-  document.getElementById('errorResidencia').textContent = 'El campo Ciudad debe ser obligatorio';
-}
-if(errores.length == 0) {
-  let datosEnvios = {
-      usuario,
-      apellidos,
-      years,
-      email,
-      telefono,
-      depart,
-      residencia
-  }
-  citas.push(datosEnvios)
-  localStorage.setItem('formulario', JSON.stringify(datos))
-}  
-})
+
+
+// const inputEnviar = document.getElementById('enviar');
+// const campoNombre = document.getElementById('nombre');
+// const campoApellido = document.getElementById('apellidos');
+// const campoEmail = document.getElementById('correo');
+// const campoTelefono = document.getElementById('celular');
+// const campoFecha = document.getElementById('edad');
+// const campoDepartamento = document.getElementById('departamento');
+// const campoResidencia = document.getElementById('ciudad');
+// const campoSi = document.getElementById('si');
+// const campoNo = document.getElementById('no');
+// const campoArchivo = document.getElementById('archivo');
+
+// inputEnviar.addEventListener('click', (evento) => {
+//   evento.preventDefault();
+
+//   let usuario = campoNombre.value;
+//   let apellido = campoApellido.value;
+//   let email = campoEmail.value;
+//   let telefono = campoTelefono.value;
+//   let edad = campoFecha.value;
+//   let departamento = campoDepartamento.value;
+//   let residencia = campoResidencia.value;
+//   let experiencia = campoSi.checked ? campoSi.value : campoNo.value;
+//   let archivo = campoArchivo.value;
+
+//   let errores = [];
+//   let datosMemoria = JSON.parse(localStorage.getItem('formulario'));
+//   let datos;
+
+//   if (!datosMemoria) {
+//     datos = [];
+//   } else {
+//     datos = datosMemoria;
+//   }
+
+//   if (!usuario) {
+//     errores.push('El nombre debe ser obligatorio');
+//     campoNombre.classList.add('is-invalid');
+//     document.getElementById('errorUsuario').textContent = 'El campo nombre debe ser obligatorio';
+//   } else if (usuario.length < 10) {
+//     campoNombre.classList.add('is-invalid');
+//     document.getElementById('errorUsuario').textContent = 'El nombre debe tener al menos 10 caracteres';
+//   } else {
+//     campoNombre.classList.remove('is-invalid');
+//   }
+
+//   if (!apellido) {
+//     errores.push('El apellido debe ser obligatorio');
+//     campoApellido.classList.add('is-invalid');
+//     document.getElementById('errorApellido').textContent = 'El campo apellido debe ser obligatorio';
+//   } else if (apellido.length < 10) {
+//     campoApellido.classList.add('is-invalid');
+//     document.getElementById('errorApellido').textContent = 'El apellido debe tener al menos 10 caracteres';
+//   } else {
+//     campoApellido.classList.remove('is-invalid');
+//   }
+
+//   if (!email) {
+//     errores.push('El correo debe ser obligatorio');
+//     campoEmail.classList.add('is-invalid');
+//     document.getElementById('errorCorreo').textContent = 'El campo correo debe ser obligatorio';
+//   } else if (!validarEmail(email)) {
+//     errores.push('El correo electrónico no es válido');
+//     campoEmail.classList.add('is-invalid');
+//     document.getElementById('errorCorreo').textContent = 'El correo electrónico no es válido';
+//   } else {
+//     campoEmail.classList.remove('is-invalid');
+//   }
+
+//   if (!telefono) {
+//     errores.push('El teléfono debe ser obligatorio');
+//     campoTelefono.classList.add('is-invalid');
+//     document.getElementById('errorTel').textContent = 'El campo Teléfono debe ser obligatorio';
+//   } else {
+//     campoTelefono.classList.remove('is-invalid');
+//   }
+
+//   if (!edad) {
+//     errores.push('La edad debe ser obligatoria');
+//     campoFecha.classList.add('is-invalid');
+//     document.getElementById('errorEdad').textContent = 'El campo edad debe ser obligatorio';
+//   } else {
+//     campoFecha.classList.remove('is-invalid');
+//   }
+
+//   if (!departamento) {
+//     errores.push('El Departamento debe ser obligatorio');
+//     campoDepartamento.classList.add('is-invalid');
+//     document.getElementById('errorDepart').textContent = 'El campo Departamento debe ser obligatorio';
+//   } else {
+//     campoDepartamento.classList.remove('is-invalid');
+//   }
+
+//   if (!residencia) {
+//     errores.push('La ciudad de residencia debe ser obligatoria');
+//     campoResidencia.classList.add('is-invalid');
+//     document.getElementById('errorResidencia').textContent = 'El campo Ciudad debe ser obligatorio';
+//   } else {
+//     campoResidencia.classList.remove('is-invalid');
+//   }
+
+//   if (!experiencia) {
+//     errores.push('La experiencia debe ser obligatoria');
+//   }
+
+//   if (!archivo) {
+//     errores.push('El archivo adjunto debe ser obligatorio');
+//     campoArchivo.classList.add('is-invalid');
+//     document.getElementById('errorArchivo').textContent = 'El campo archivo adjunto debe ser obligatorio';
+//   } else {
+//     campoArchivo.classList.remove('is-invalid');
+//   }
+
+//   if (errores.length === 0) {
+//     let datosEnvio = {
+//       usuario,
+//       apellido,
+//       email,
+//       telefono,
+//       edad,
+//       departamento,
+//       residencia,
+//       experiencia,
+//       archivo
+//     };
+
+//     datos.push(datosEnvio);
+//     localStorage.setItem('formulario', JSON.stringify(datos));
+//   }
+// });
+
+// function validarEmail(email) {
+//   // Expresión regular para validar un correo electrónico
+//   const re = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+//   return re.test(email);
+// }
--- js/validador.js
+++ js/validador.js
@@ -1,51 +1,204 @@
-let memoria = JSON.parse(localStorage.getItem("formularios"));
+let memoria =JSON.parse(localStorage.getItem("formularios"));
 
-let contenedor = document.getElementById("contenedor");
+let contenedor = document.getElementById("container");
 
-memoria.forEach(function(datos){
 
-    let fila=document.createElement("section.container")
-    fila.classList.add("row","my-5","shadow","text-center","p-5")
+// Verificar si el contenedor existe antes de agregar las filas
+if (contenedor) {
+  memoria.array.forEach(function(datosMemoria) { 
+    // Código para crear y agregar filas
+    let fila = document.createElement("section");
+  fila.classList.add("container", "row", "my-5", "shadow", "text-center", "p-5");
 
-    let columnaUno=document.createElement("section.izq")
-    columnaUno.classList.add("col-4")
+  let columnaUno = document.createElement("section");
+  columnaUno.classList.add("col-4");
 
-    let columnaDos=document.createElement("section.der")
-    columnaDos.classList.add("col-8","border-start","p-4")
+  let columnaDos = document.createElement("section");
+  columnaDos.classList.add("col-8", "border-start", "p-4");
 
-    let titulo=document.createElement("h1")
-    titulo.textContent="Personal Inscrito"
+  let titulo = document.createElement("h1");
+  titulo.textContent = "Personal Inscrito";
 
-    let usuario=document.createElement("h3")
-    usuario.textContent= +datos.usuario
+  let usuario = document.createElement("h3");
+  usuario.textContent = "Nombre: " + datos.usuario;
 
-    let lastname=document.createElement("p")
-    documento.textContent=+datos.apellidos
+  let lastname = document.createElement("p");
+  lastname.textContent = "Apellidos: " + datos.apellido;
 
-    let email=document.createElement("p")
-    email.textContent="Email: "+datos.correo
+  let email = document.createElement("p");
+  email.textContent = "Email: " + datos.email;
 
-    let telefono=document.createElement("p")
-    telefono.textContent="Número de Celular: "+datos.celular
+  let telefono = document.createElement("p");
+  telefono.textContent = "Número de Celular: " + datos.telefono;
 
-    let years=document.createElement("p")
-    years.textContent="La Edad es: "+datos.edad
+  let years = document.createElement("p");
+  years.textContent = "La Edad es: " + datos.edad;
 
-    let depart=document.createElement("p")
-    depart.textContent="Departamento: "+datos.departamento
+  let depart = document.createElement("p");
+  depart.textContent = "Departamento: " + datos.departamento;
 
-    let residencia=document.createElement("p")
-    residencia.textContent="Ciudad: "+datos.ciudad
+  let residencia = document.createElement("p");
+  residencia.textContent = "Ciudad: " + datos.residencia;
 
-    columnaUno.appendChild(titulo)
-    columnaDos.appendChild(usuario)
-    columnaDos.appendChild(lastname)
-    columnaDos.appendChild(email)
-    columnaDos.appendChild(telefono)
-    columnaDos.appendChild(years)
-    columnaDos.appendChild(editar);
-    columnaDos.appendChild(cancelar);
-    fila.appendChild(columnaUno)
-    fila.appendChild(columnaDos)
-    contenedor.appendChild(fila)
-})
+    // Agregar la fila al contenedor
+    columnaUno.appendChild(titulo);
+      columnaDos.appendChild(usuario);
+      columnaDos.appendChild(lastname);
+      columnaDos.appendChild(email);
+      columnaDos.appendChild(telefono);
+      columnaDos.appendChild(years);
+      columnaDos.appendChild(depart);
+      columnaDos.appendChild(residencia);
+      fila.appendChild(columnaUno);
+      fila.appendChild(columnaDos);
+    contenedor.appendChild(fila);
+  });
+} else {
+  console.error("No se pudo encontrar el elemento contenedor");
+}
+
+
+
+// memoria.forEach(function (datos) {
+//   let fila = document.createElement("section");
+//   fila.classList.add("container", "row", "my-5", "shadow", "text-center", "p-5");
+
+//   let columnaUno = document.createElement("section");
+//   columnaUno.classList.add("col-4");
+
+//   let columnaDos = document.createElement("section");
+//   columnaDos.classList.add("col-8", "border-start", "p-4");
+
+//   let titulo = document.createElement("h1");
+//   titulo.textContent = "Personal Inscrito";
+
+//   let usuario = document.createElement("h3");
+//   usuario.textContent = "Nombre: " + datos.usuario;
+
+//   let lastname = document.createElement("p");
+//   lastname.textContent = "Apellidos: " + datos.apellido;
+
+//   let email = document.createElement("p");
+//   email.textContent = "Email: " + datos.email;
+
+//   let telefono = document.createElement("p");
+//   telefono.textContent = "Número de Celular: " + datos.telefono;
+
+//   let years = document.createElement("p");
+//   years.textContent = "La Edad es: " + datos.edad;
+
+//   let depart = document.createElement("p");
+//   depart.textContent = "Departamento: " + datos.departamento;
+
+//   let residencia = document.createElement("p");
+//   residencia.textContent = "Ciudad: " + datos.residencia;
+
+//   columnaUno.appendChild(titulo);
+//   columnaDos.appendChild(usuario);
+//   columnaDos.appendChild(lastname);
+//   columnaDos.appendChild(email);
+//   columnaDos.appendChild(telefono);
+//   columnaDos.appendChild(years);
+//   columnaDos.appendChild(depart);
+//   columnaDos.appendChild(residencia);
+//   fila.appendChild(columnaUno);
+//   fila.appendChild(columnaDos);
+//   contenedor.appendChild(fila);
+// });
+
+
+// memoria.forEach((datosMemoria) => {
+//     let fila=document.createElement("div")
+//     fila.classList.add("row","my-5","shadow","text-center","p-5")
+
+//     let columnaUno=document.createElement("div")
+//     columnaUno.classList.add("col-4")
+
+//     let columnaDos=document.createElement("div")
+//     columnaDos.classList.add("col-8","border-start","p-4")
+
+//     let titulo=document.createElement("h1")
+//     titulo.textContent="Personal Inscrito"
+
+//     let usuario=document.createElement("h3")
+//     usuario.textContent= +datos.usuario
+
+//     let lastname=document.createElement("p")
+//     documento.textContent=+datos.apellidos
+
+//     let email=document.createElement("p")
+//     email.textContent="Email: "+datos.correo
+
+//     let telefono=document.createElement("p")
+//     telefono.textContent="Número de Celular: "+datos.celular
+
+//     let years=document.createElement("p")
+//     years.textContent="La Edad es: "+datos.edad
+
+//     let depart=document.createElement("p")
+//     depart.textContent="Departamento: "+datos.departamento
+
+//     let residencia=document.createElement("p")
+//     residencia.textContent="Ciudad: "+datos.ciudad
+
+//     columnaUno.appendChild(titulo)
+//     columnaDos.appendChild(usuario)
+//     columnaDos.appendChild(lastname)
+//     columnaDos.appendChild(email)
+//     columnaDos.appendChild(telefono)
+//     columnaDos.appendChild(years)
+//     columnaDos.appendChild(editar);
+//     columnaDos.appendChild(cancelar);
+//     fila.appendChild(columnaUno)
+//     fila.appendChild(columnaDos)
+//     contenedor.appendChild(fila)
+    
+// });
+
+// memoria.forEach(function(datosMemoria){
+
+//     let fila=document.createElement("div")
+//     fila.classList.add("row","my-5","shadow","text-center","p-5")
+
+//     let columnaUno=document.createElement("div")
+//     columnaUno.classList.add("col-4")
+
+//     let columnaDos=document.createElement("div")
+//     columnaDos.classList.add("col-8","border-start","p-4")
+
+//     let titulo=document.createElement("h1")
+//     titulo.textContent="Personal Inscrito"
+
+//     let usuario=document.createElement("h3")
+//     usuario.textContent= +datos.usuario
+
+//     let lastname=document.createElement("p")
+//     documento.textContent=+datos.apellidos
+
+//     let email=document.createElement("p")
+//     email.textContent="Email: "+datos.correo
+
+//     let telefono=document.createElement("p")
+//     telefono.textContent="Número de Celular: "+datos.celular
+
+//     let years=document.createElement("p")
+//     years.textContent="La Edad es: "+datos.edad
+
+//     let depart=document.createElement("p")
+//     depart.textContent="Departamento: "+datos.departamento
+
+//     let residencia=document.createElement("p")
+//     residencia.textContent="Ciudad: "+datos.ciudad
+
+//     columnaUno.appendChild(titulo)
+//     columnaDos.appendChild(usuario)
+//     columnaDos.appendChild(lastname)
+//     columnaDos.appendChild(email)
+//     columnaDos.appendChild(telefono)
+//     columnaDos.appendChild(years)
+//     columnaDos.appendChild(editar);
+//     columnaDos.appendChild(cancelar);
+//     fila.appendChild(columnaUno)
+//     fila.appendChild(columnaDos)
+//     contenedor.appendChild(fila)
+// })
